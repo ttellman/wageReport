de7842d569217e9d34bca5a80c693a2c14bc3fcc
ttellman <ttellmandesign@gmail.com>
2013-07-17 21:55:43 -0500

    Adding new files
    added files

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tim
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      WageForm form = new WageForm();
+        form.setVisible(true);  // calls the Wageform 
+    }
+}
